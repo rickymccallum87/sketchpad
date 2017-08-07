0d61250cbab4c897df56fd3e931dea1a07133770
Ricky McCallum <rickymccallum87@gmail.com>
2017-08-07 16:20:58 +0000

    Implement grid and reset button

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,49 @@
 /* global $ */
+
 $(document).ready(function() {
-  var grid = $('.grid');
-  var row = 1;
-  for (var i = 0; i < 16; i++) {
-    grid.append('<div class="square new-row"></div>');
-    for (var j = 0; j < 15; j++) {
-      grid.append('<div class="square"></div>');
+  var grid = $('#grid');
+  var squareCount = 16; 
+  var gridSize = 800;
+  
+  drawGrid(squareCount);
+  
+  // Create a grid of 16x16 divs
+  function drawGrid(sq) {
+    for (var i = 0; i < sq; i++) {
+      addSquare(true);
+      for (var j = 0; j < sq - 1; j++) {
+        addSquare();
+      }
     }
+    sizeGrid();
   }
+  
+  // Create a square, on new row if specified
+  function addSquare(newRow) {
+    if (newRow)
+      grid.append('<div class="square new-row"></div>');
+    else
+      grid.append('<div class="square"></div>');
+  }
+  
+  // Resize grid to original dimensions
+  function sizeGrid() {
+   var eachSize = gridSize / squareCount;
+    $('.square').css({
+      'height': eachSize,
+      'width': eachSize
+    });
+  }
+  
+  // Allow user to draw over grid
+  grid.on('mouseenter', '.square', function() {
+    $(this).addClass('activated');
+  }); 
+  
+  // Reset grid
+  $('#reset').on('click', function() {
+    squareCount = window.prompt("How many squares per side?", 64);
+    grid.text('');
+    drawGrid(squareCount);
+  }); 
 });
