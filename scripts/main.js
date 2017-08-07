9fb4f8f67d88c2a033aa5fe262f5c45889bec697
Ricky McCallum <rickymccallum87@gmail.com>
2017-08-07 00:41:24 +0000

    Create grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,11 @@
 /* global $ */
 $(document).ready(function() {
-  
+  var grid = $('.grid');
+  var row = 1;
+  for (var i = 0; i < 16; i++) {
+    grid.append('<div class="square new-row"></div>');
+    for (var j = 0; j < 15; j++) {
+      grid.append('<div class="square"></div>');
+    }
+  }
 });
